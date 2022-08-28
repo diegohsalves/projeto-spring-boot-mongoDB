953838cacd864f1f95ecd1b7036eba360df05ab3
Diego Alves <diegohsalves@gmail.com>
2022-08-28 12:16:15 -0300

    Consulta simples com query methods

--- src/main/java/com/workshopmongo/services/PostService.java
+++ src/main/java/com/workshopmongo/services/PostService.java
@@ -1,5 +1,7 @@
 package com.workshopmongo.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -22,6 +24,10 @@ public class PostService {
 		} else {
 			return post;
 		}
+	}
+	
+	public List<Post> findByTitle(String text) {
+		return repository.findByTitleContaining(text);
 	}
 
 }
